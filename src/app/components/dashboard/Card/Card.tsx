00183f76a6d43850d7f96601b181e9c0868d1913
mathiasck <mathias.ck2000@gmail.com>
2023-11-17 09:44:44 +0100

    Style dashboard page

--- src/app/components/dashboard/Card/Card.tsx
+++ src/app/components/dashboard/Card/Card.tsx
@@ -1,5 +1,18 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./Card.module.css";
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={34} />
+      <div className={styles.texts}>
+        <span>Total users</span>
+        <span>10</span>
+        <span>
+          <span className={styles.positive}>12%</span> more than previous week
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,23 @@
+import { Card, Chart, Transactions } from "../components/dashboard";
+import { RightBar } from "../components/ui";
+import styles from "./dashboard.module.css";
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
