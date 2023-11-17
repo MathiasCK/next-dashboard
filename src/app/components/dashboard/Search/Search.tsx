9868b2be52d54bdf7142c60bcedbdea3a7e83e8b
mathiasck <mathias.ck2000@gmail.com>
2023-11-17 13:59:01 +0100

    Style users page

--- src/app/components/dashboard/Search/Search.tsx
+++ src/app/components/dashboard/Search/Search.tsx
@@ -1,5 +1,18 @@
-const Search = () => {
-  return <div>Search</div>;
+import { FC } from "react";
+import styles from "./search.module.css";
+import { MdSearch } from "react-icons/md";
+
+interface Props {
+  placeholder: string;
+}
+
+const Search: FC<Props> = ({ placeholder }) => {
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input type="text" placeholder={placeholder} className={styles.input} />
+    </div>
+  );
 };
 
 export default Search;
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,5 +1,62 @@
+import { Pagination, Search } from "@/app/components/dashboard";
+import styles from "./users.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user ..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                  alt=""
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13.01.2022</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <Link href="/">
+                <button className={`${styles.button} ${styles.view}`}>
+                  View
+                </button>
+              </Link>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default UsersPage;
