6ffcb4be228ddc26da8907cc7b3c8785a9de6848
mathiasck <mathias.ck2000@gmail.com>
2023-11-21 08:21:39 +0100

    Handle user search

--- src/app/components/dashboard/Search/Search.tsx
+++ src/app/components/dashboard/Search/Search.tsx
@@ -1,16 +1,43 @@
-import { FC } from "react";
+"use client";
+import { FC, ChangeEvent } from "react";
 import styles from "./search.module.css";
 import { MdSearch } from "react-icons/md";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 interface Props {
   placeholder: string;
 }
 
 const Search: FC<Props> = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const params = new URLSearchParams(searchParams);
+
+      if (e.target.value) {
+        params.set("q", e.target.value);
+      } else {
+        params.delete("q");
+      }
+
+      replace(`${pathname}?${params.toString()}`);
+    },
+    300,
+  );
+
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -2,8 +2,19 @@ import { Pagination, Search } from "@/app/components/dashboard";
 import styles from "./users.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import { fetchUsers } from "@/app/utils/data";
+import { IUser } from "@/app/utils/models";
+import { NextPage } from "next";
 
-const UsersPage = () => {
+interface Props {
+  searchParams: {
+    q?: string;
+  };
+}
+const UsersPage: NextPage<Props> = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const users: IUser[] = await fetchUsers(q);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -24,34 +35,37 @@ const UsersPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                  alt=""
-                />
-                John Doe
-              </div>
-            </td>
-            <td>john@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td>
-              <Link href="/dashboard/users/1">
-                <button className={`${styles.button} ${styles.view}`}>
-                  View
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                    alt=""
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              {/* @ts-ignore */}
+              <td>{user.createdAt.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Passive"}</td>
+              <td>
+                <Link href={`/dashboard/users/${user.id}`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
                 </button>
-              </Link>
-              <button className={`${styles.button} ${styles.delete}`}>
-                Delete
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
